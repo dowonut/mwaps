0d11a27f29e838324f1b11960c151f7e8f25f5f0
Linus Graaskov <linusgraaskov@gmail.com>
2024-04-22 21:20:10 +0200

    added icons and updated readme

---
+++ components/Map.tsx
@@ -0,0 +1,137 @@
+import React, { CSSProperties } from "react";
+import "./Map.css";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const center: google.maps.LatLngLiteral = {
+  lat: 35.6985493,
+  lng: 139.7731162,
+};
+
+const options: google.maps.MapOptions = {
+  backgroundColor: "black",
+  disableDefaultUI: true,
+  zoom: 10,
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyD4VJoXentlADWx9vI1l_On2kgYHi0MRrQ",
+  });
+
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+
+  // On map load
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    // Define custom dark mode map style
+    const darkMode = new google.maps.StyledMapType(
+      [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+      ],
+      { name: "Dark Mode" }
+    );
+
+    // map.mapTypes.set("dark_mode", darkMode);
+    // map.setMapTypeId("dark_mode");
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      center={center}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      mapContainerClassName="google-map"
+      options={options}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(Map);
